3aff9236fad7b75016d057ca8a2e41a76ea7ffca
Kieran Willis <kieranwillisx@gmail.com>
2020-01-15 10:52:28 +0000

    Added pre build script back with commented out code

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# echo "iOS: Installing applesimutils..."
+# brew tap wix/brew
+# brew update
+# brew install wix/brew/applesimutils --HEAD
+
+
+# echo "iOS: Installing detox cli..."
+# npm install -g detox-cli
+# npm install -g detox
+
+# cd ios && pod deintegrate && pod install && cd ..
+
+# echo "iOS: Building the project..."
+# detox build --configuration ios.release
+
+# echo "iOS: Cleaning cache..."
+# # detox clean-framework-cache && detox build-framework-cache
+
+# echo "iOS: Executing tests..."
+# detox test --configuration ios.release --cleanup
